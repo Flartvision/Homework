11c45ec6e41d72aa0d98d5c8ab9ddb7adb07fc89
Flart <daniil-fedor@mail.ru>
2024-08-24 00:54:38 +0300

    Завершённый варианта convertor

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,24 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const U2E = 0.90145
 const U2R = 89.46
 
 func main() {
-	val, cur1, cur2 := getUserInput()
-	res := calcCur()
+	cur1, valConv, cur2 := getUserInput()
+
+	calcCur(valConv, cur1, cur2)
+
+}
+
+func getUserInput() (string, float64, string) {
+	var cur1 string
+	var valConv float64
+	var cur2 string
+	for {
+		fmt.Println("Введите исходную валюту (eur, usd или rub):")
+		fmt.Scanln(&cur1)
+
+		if !checkCurIn(&cur1) {
+			fmt.Println("Некорректно введена исходная валюта")
+			continue
+		}
+
+		break
+	}
+
+	for {
+
+		fmt.Println("Введите  валюту (Числовой параметр):")
+		_, err := fmt.Scan(&valConv)
+		fmt.Println(valConv)
+		isFloat := fmt.Sprintf("%T", valConv)
+
+		if err != nil || isFloat != "float64" {
+			fmt.Println("Некорректно введено значение")
+			continue
+		}
+
+		break
+	}
+
+	for {
+		fmt.Println("Введите целевую валюту (eur, usd или rub):")
+		fmt.Scanln(&cur2)
+
+		if !checkCurIn(&cur2) {
+			fmt.Println("Некорректно введена целевая валюта")
+			continue
+		}
+
+		if cur1 == cur2 {
+			fmt.Println("Исходная и целевая валюты совпадают.")
+			continue
+		}
+
+		break
+	}
+
+	return strings.ToLower(cur1), valConv, strings.ToLower(cur2)
+
+}
+
+func calcCur(val float64, cur1, cur2 string) {
 	E2R := U2R / U2E
-	fmt.Println(E2R)
+
+	switch {
+	case cur1 == "usd" && cur2 == "eur":
+		fmt.Printf("%.3f \n", val*U2E)
+	case cur1 == "eur" && cur2 == "usd":
+		fmt.Printf("%.3f \n", val/U2E)
+	case cur1 == "rub" && cur2 == "usd":
+		fmt.Printf("%.3f \n", val/U2R)
+	case cur1 == "usd" && cur2 == "rub":
+		fmt.Printf("%.3f \n", val*U2R)
+	case cur1 == "rub" && cur2 == "eur":
+		fmt.Printf("%.3f \n", val/E2R)
+	case cur1 == "eur" && cur2 == "rub":
+		fmt.Printf("%.3f \n", val*E2R)
+	}
+
 }
 
-func getUserInput() (string, string, string) {
-	val := fmt.Sprintln("Введите  валюту (EUR, USD или RUB):")
-	cur1 := fmt.Sprintln("Введите исходную валюту (EUR, USD или RUB):")
-	cur2 := fmt.Sprint("Введите  целевую валюту (EUR, USD или RUB):")
-	return val, cur1, cur2
-}
+func checkCurIn(pCur *string) bool {
+	cur := strings.ToLower(*pCur)
 
-func calcCur(val float64, cur1, cur2 string) float64 {
+	if cur == "usd" || cur == "eur" || cur == "rub" {
+		return true
+	}
+
+	return false
 
 }
